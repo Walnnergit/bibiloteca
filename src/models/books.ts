a572fb9e4a93b2239aae5230d26ec11ae57b374b
Soy Walnner <walnerm75@gmail.com>
2026-05-22 21:05:39 -0600

    commit

--- src/models/books.ts
+++ src/models/books.ts
@@ -1,13 +1,60 @@
 export class Book {
-    private id:number;
-    private title:string;
-    private author:string;
-    private disponible:boolean = false;
 
-    constructor(id:number, title:string, author:string, disponible:boolean){
+    //Properties
+    private id: string;
+    private title: string;
+    private author: string;
+    private available: boolean;
+
+    constructor(id: string,
+        title: string,
+        author: string,
+        available: boolean){
         this.id = id;
         this.title = title;
         this.author = author;
-
+        this.available = available;
     }
+
+    //gettter
+    getId(): string {
+        return this.id;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    getAuthor(): string {
+        return this.author;
+    }
+
+    //Setter
+    setTitle(title: string): void {
+        this.title = title;
+    }
+
+    setAuthor(author: string): void {
+        this.author = author;
+    }
+
+    setId(id: string): void {
+        this.id = id;
+    }
+    //Estado disponible
+    conditionOfDaBook(): void {
+        if(this.available = true)
+        console.log(`El libro con ID ${this.id} esta disponible`)
+        }
+
+    else(){
+        console.log(`El libro con ID ${this.id} no esta disponible`)
+    }
+    //Desactivar o activar libro
+    canbiardisponibilidadellibro(nuevadisponibilidad: boolean): void {
+        this.available = nuevadisponibilidad;
+    }
+
 }
+
+
